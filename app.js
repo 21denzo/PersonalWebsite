1dd6b1cc45e3f861d1a2ea4400317ced89a1a012
Denzel <denzel.opoku@yahoo.com>
2026-09-08 02:46:05 +0200

    scrolling behaviour

--- app.js
+++ app.js
@@ -9,3 +9,26 @@ const mobileMenu = () => {
 }
 
 menu.addEventListener('click', mobileMenu);
+
+
+
+//"To Top" Button
+
+const scrollBtn = document.getElementById("scroll_up");
+
+// Show button after scrolling down 200px
+window.addEventListener("scroll", () => {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        scrollBtn.style.display = "block";
+    } else {
+        scrollBtn.style.display = "none";
+    }
+});
+
+// Scroll to top smoothly when clicked
+scrollBtn.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
